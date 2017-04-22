e5b91dead5ba3cd7e232d101dd1e9207fd245c9a
zamoroka <zapashok0@gmail.com>
2017-04-22 20:40:56 +0300

    add license

--- app/code/local/Zamoroka/Issuu/Model/Api/Response.php
+++ app/code/local/Zamoroka/Issuu/Model/Api/Response.php
@@ -1,10 +1,29 @@
 <?php
+/**
+ * Zamoroka_Issuu
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to p@zamoroka.com so we can send you a copy immediately.
+ *
+ * @category  Issuu
+ * @package   Zamoroka
+ * @copyright Copyright (c) 2017 Zamoroka Pavlo (http://www.zamoroka.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 
 /**
- * Created by PhpStorm.
- * User: zamoroka
- * Date: 22.04.2017
- * Time: 17:50
+ * Zamoroka_Issuu_Model_Api_Response
+ *
+ * @category Issuu
+ * @package  Zamoroka
+ * @author   Zamoroka Pavlo
  */
 class Zamoroka_Issuu_Model_Api_Response implements Zamoroka_Issuu_Model_Api_Interface_Response
 {
